85e1418d021a568d58e53cb4d92551fb8e3460fc
Joesus <joesusnick@gmail.com>
2017-01-21 14:11:20 -0700

    more tests that don’t pass

--- AllisonsCatPalaceTDD/AllisonsCatPalaceTDDTests/CatNetworkerTests.swift
+++ AllisonsCatPalaceTDD/AllisonsCatPalaceTDDTests/CatNetworkerTests.swift
@@ -14,7 +14,25 @@ class CatNetworkerTests: XCTestCase {
         XCTAssertEqual(CatNetworker.session, URLSession.shared, "Networker should be using the shared session")
     }
 
-    func testCreatingRetrieveAllCatsTask() {}
+    func testCreatingRetrieveAllCatsTask() {
+        CatNetworker.retrieveAllCats(success: {}, failure: {})
+
+        var potentialTask: URLSessionDataTask?
+        CatNetworker.session.getAllTasks { tasks in
+            potentialTask = tasks[0] as? URLSessionDataTask
+        }
+        guard let task = potentialTask else {
+            return XCTFail("A task should be created for retrieving all cats")
+        }
+
+        guard let request = task.currentRequest else {
+            return XCTFail("A task should have a currentRequest")
+        }
+
+        XCTAssertEqual(request.httpMethod, "GET", "The request method for retrieving a cat should be get")
+        XCTAssertEqual(request.url?.host, "firebase.com", "The domain should be firebase")
+        XCTAssertEqual(request.url?.path, "/cats", "The path should be cats")
+    }
 
     func testNewRetrieveAllCatsTaskCancelsExistingTask() {}
 }

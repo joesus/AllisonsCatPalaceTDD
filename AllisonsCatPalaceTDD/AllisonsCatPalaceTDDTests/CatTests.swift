8bb0355bfd9b7318ec6f2372a340ef669d15ad30
joesus <joesusnick@gmail.com>
2017-02-26 12:09:01 -0700

    adds test for creating cat with urlString

--- AllisonsCatPalaceTDD/AllisonsCatPalaceTDDTests/CatTests.swift
+++ AllisonsCatPalaceTDD/AllisonsCatPalaceTDDTests/CatTests.swift
@@ -12,9 +12,15 @@ import XCTest
 class CatTests: XCTestCase {
 
     let cat = Cat(name: "CatOne", identifier: 1)
+    let catWithURLString = Cat(name: "CatURL", identifier: 1, urlString: "http://example.com/foo.png")
 
     func testCreateCat() {
         XCTAssertEqual(cat.name, "CatOne", "Cat name was set incorrectly")
         XCTAssertEqual(cat.identifier, 1, "Cat Id was set incorrectly")
+    }
+
+    func testCanCreateCatWithURLString() {
+        XCTAssertNotNil(catWithURLString.urlString)
+        XCTAssertEqual(catWithURLString.urlString, "http://example.com/foo.png", "Cat urlString was set incorrectly")
     }
 }

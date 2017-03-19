04b948d9b7e7eea3bdbfcc4b20b319f8d64c3ab5
joesus <joesusnick@gmail.com>
2017-03-19 14:31:37 -0600

    adds type and tests for Gender

---
+++ AllisonsCatPalaceTDD/AllisonsCatPalaceTDD/Gender.swift
@@ -0,0 +1,13 @@
+//
+//  Gender.swift
+//  AllisonsCatPalaceTDD
+//
+//  Created by Joesus on 3/19/17.
+//  Copyright © 2017 Joesus. All rights reserved.
+//
+
+import Foundation
+
+enum Gender {
+    case unknown, male, female
+}

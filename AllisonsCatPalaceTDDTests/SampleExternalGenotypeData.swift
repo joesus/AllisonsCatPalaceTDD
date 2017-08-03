70d23fdba6191972a083eac5878c407ab6ca2010
Joe Susnick <joesusnick@gmail.com>
2017-08-02 18:28:19 -0600

    adds sample external genotype

---
+++ AllisonsCatPalaceTDDTests/SampleExternalGenotypeData.swift
@@ -0,0 +1,85 @@
+//
+//  SampleExternalGenotypeData.swift
+//  AllisonsCatPalaceTDD
+//
+//  Created by Joe Susnick on 7/29/17.
+//  Copyright © 2017 Joesus. All rights reserved.
+//
+
+import Foundation
+@testable import AllisonsCatPalaceTDD
+
+typealias ExternalGenotype = JsonObject
+
+struct SampleExternalGenotypeData {
+    static let catSpecies: JsonObject = [
+        "$t": "Cat"
+    ]
+    static let mixed: JsonObject = [
+        "$t": "yes"
+    ]
+    static let purebred: JsonObject = [
+        "$t": "no"
+    ]
+    static let validBreed: JsonObject = [
+        "breed": [
+            [
+                "$t": "Domestic Short Hair (Black & White)"
+            ]
+        ]
+    ]
+    static let validBreeds: JsonObject = [
+        "breed": [
+            [
+                "$t": "Domestic Short Hair (Black & White)"
+            ],
+            [
+                "$t": "Tabby (Orange)"
+            ]
+        ]
+    ]
+    static let validMixed: ExternalGenotype = [
+        "animal": catSpecies,
+        "mix": mixed,
+        "breeds": validBreeds
+    ]
+    static let validPurebred: ExternalGenotype = [
+        "animal": catSpecies,
+        "mix": purebred,
+        "breeds": validBreed
+    ]
+    static let missingSpecies: ExternalGenotype = [
+        "mix": mixed,
+        "breeds": validBreeds
+    ]
+    static let emptySpecies: ExternalGenotype = [
+        "animal": [],
+        "mix": mixed,
+        "breeds": validBreeds
+    ]
+    static let missingPurity: ExternalGenotype = [
+        "animal": catSpecies,
+        "breeds": validBreeds
+    ]
+    static let emptyPurity: ExternalGenotype = [
+        "animal": catSpecies,
+        "mix": [],
+        "breeds": validBreeds
+    ]
+    static let missingBreedsElement: ExternalGenotype = [
+        "animal": catSpecies,
+        "mix": mixed
+    ]
+    static let missingBreedElement: ExternalGenotype = [
+        "animal": catSpecies,
+        "mix": mixed,
+        "breeds": []
+    ]
+    static let emptyBreedElement: ExternalGenotype = [
+        "animal": catSpecies,
+        "mix": mixed,
+        "breeds": [
+            "breed": []
+        ]
+    ]
+}

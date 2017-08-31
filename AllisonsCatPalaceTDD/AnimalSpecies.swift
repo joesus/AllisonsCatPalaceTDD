b719ba361ea7599e56507bbf224ca41730e15f83
Joe Susnick <joesusnick@gmail.com>
2017-08-31 13:25:03 -0600

    adds persistence for species

--- AllisonsCatPalaceTDD/AnimalSpecies.swift
+++ AllisonsCatPalaceTDD/AnimalSpecies.swift
@@ -8,18 +8,35 @@
 
 import Foundation
 
-enum AnimalSpecies {
-    case cat, dog
+enum AnimalSpecies: Int {
+    case cat, dog, other
 
     private static let petFinderRawValueMappable: [String: AnimalSpecies] = [
         "Cat": .cat,
         "Dog": .dog
     ]
 
-    init?(petFinderRawValue: String) {
+    init(petFinderRawValue: String) {
         guard let value = AnimalSpecies.petFinderRawValueMappable[petFinderRawValue] else {
-            return nil
+            self = .other
+            return
         }
         self = value
+    }
+}
+
+extension AnimalSpecies: Persistable {
+    typealias ManagedObject = ManagedIntObject
+
+    var managedObject: ManagedIntObject {
+        let object = ManagedIntObject()
+
+        object.value = self.rawValue
+
+        return object
+    }
+
+    init?(managedObject: ManagedObject) {
+        self.init(rawValue: managedObject.value)
     }
 }
--- AllisonsCatPalaceTDDTests/AnimalSpeciesTests.swift
+++ AllisonsCatPalaceTDDTests/AnimalSpeciesTests.swift
@@ -12,16 +12,16 @@ import XCTest
 class AnimalSpeciesTests: XCTestCase {
 
     func testInitializerWithEmptyString() {
-        XCTAssertNil(AnimalSpecies(petFinderRawValue: ""),
-                     "Should not create animal species from empty string")
+        XCTAssertEqual(AnimalSpecies(petFinderRawValue: ""), .other,
+                     "Should create animal species other from empty string")
     }
 
     func testInitializerWithInvalidStrings() {
         let strings = ["dog", "cat", "DOG", "CAT", "BarnYard", "Bird", "Horse", "Rabbit", "Reptile"]
 
         strings.forEach { character in
-            XCTAssertNil(AnimalSpecies(petFinderRawValue: character),
-                         "\(character) should not create an animal species")
+            XCTAssertEqual(AnimalSpecies(petFinderRawValue: ""), .other,
+                           "\(character) should create animal species other")
         }
     }
 
@@ -37,10 +37,25 @@ class AnimalSpeciesTests: XCTestCase {
 
         animalSpecies.forEach { species in
             switch species {
-            case .cat, .dog:
+            case .cat, .dog, .other:
                 break
             }
         }
     }
 
+    func testManagedObject() {
+        let species = AnimalSpecies.cat
+        let managed = species.managedObject
+
+        XCTAssertEqual(species.rawValue, managed.value,
+                       "Managed object should store correct raw value")
+    }
+
+    func testInitializingFromManagedObject() {
+        let species = AnimalSpecies.dog
+        let managed = species.managedObject
+        let objectFromManaged = AnimalSpecies(managedObject: managed)
+
+        XCTAssertEqual(objectFromManaged?.rawValue, species.rawValue)
+    }
 }

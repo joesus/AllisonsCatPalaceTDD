c7689c9756054ad35880e76b4dcce49d9b23bd67
Joe Susnick <joesusnick@gmail.com>
2017-09-05 16:12:55 -0400

    persists animal

--- AllisonsCatPalaceTDD/Animal.swift
+++ AllisonsCatPalaceTDD/Animal.swift
@@ -7,8 +7,27 @@
 //
 
 import Foundation
+import RealmSwift
 
-class Animal {
+class AnimalObject: Object {
+    dynamic var name: String? = nil
+    var identifier = RealmOptional<Int>()
+    dynamic var about: String? = nil
+    dynamic var adoptionStatus: AnimalAdoptionStatusObject?
+    dynamic var age: AnimalAgeGroupObject?
+    dynamic var city: String? = nil
+    dynamic var sex: AnimalSexObject?
+    dynamic var genotype: AnimalGenotypeObject?
+    dynamic var stateCode: String? = nil
+    dynamic var size: AnimalSizeObject?
+    dynamic var imageLocations: AnimalImageLocationsObject?
+
+    override class func primaryKey() -> String? {
+        return "identifier"
+    }
+}
+
+final class Animal {
 
     let name: String
     let identifier: Int
@@ -58,5 +77,61 @@ class Animal {
     init(name: String, identifier: Int, imageUrl: URL? = nil) {
         self.name = name
         self.identifier = identifier
+    }
+}
+
+extension Animal: Persistable {
+    typealias ManagedObject = AnimalObject
+
+    var managedObject: ManagedObject {
+        let object = ManagedObject()
+
+        object.name = name
+        object.identifier = RealmOptional<Int>(identifier)
+        object.about = about
+        object.adoptionStatus = adoptionStatus?.managedObject
+        object.age = age?.managedObject
+        object.city = city
+        object.sex = sex.managedObject
+        object.genotype = genotype?.managedObject
+        object.stateCode = stateCode
+        object.size = size?.managedObject
+        object.imageLocations = imageLocations.managedObject
+
+        return object
+    }
+
+    convenience init?(managedObject: ManagedObject) {
+
+        guard let name = managedObject.name,
+            let identifier = managedObject.identifier.value else {
+                return nil
+        }
+
+        self.init(name: name, identifier: identifier)
+        if let statusObject = managedObject.adoptionStatus {
+            adoptionStatus = AnimalAdoptionStatus(managedObject: statusObject)
+        }
+
+        if let ageObject = managedObject.age {
+            age = AnimalAgeGroup(managedObject: ageObject)
+        }
+
+        city = managedObject.city
+
+        if let sexObject = managedObject.sex,
+            let sex = AnimalSex(managedObject: sexObject) {
+            self.sex = sex
+        }
+
+        if let genotypeObject = managedObject.genotype {
+            genotype = AnimalGenotype(managedObject: genotypeObject)
+        }
+
+        stateCode = managedObject.stateCode
+
+        if let sizeObject = managedObject.size {
+            size = AnimalSize(managedObject: sizeObject)
+        }
     }
 }

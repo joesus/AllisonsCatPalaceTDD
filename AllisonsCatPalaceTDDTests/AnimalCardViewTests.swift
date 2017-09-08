c3a11f57bcaf6f400a2cfcda794b56e082b35cb2
Joe Susnick <joesusnick@gmail.com>
2017-09-08 16:21:32 -0400

    todo downgraded

--- AllisonsCatPalaceTDDTests/AnimalCardViewTests.swift
+++ AllisonsCatPalaceTDDTests/AnimalCardViewTests.swift
@@ -147,7 +147,7 @@ class AnimalCardViewTests: XCTestCase {
     }
 }
 
-// TODO - tests to make sure it's setting images on main thread.
+// Nice to have - tests to make sure it's setting images on main thread.
 
 extension AnimalCardViewTests {
     func setupComponents() {

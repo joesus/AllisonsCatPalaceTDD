dd99cb4afa49020953e2f543bdcde90243450911
Joe Susnick <joesusnick@gmail.com>
2017-10-14 14:08:13 -0600

    Adds dependent type for zip code

---
+++ AllisonsCatPalaceTDD/ZipCode.swift
@@ -0,0 +1,23 @@
+//
+//  ZipCode.swift
+//  AllisonsCatPalaceTDD
+//
+//  Created by Joe Susnick on 10/14/17.
+//  Copyright © 2017 Joesus. All rights reserved.
+//
+
+import Foundation
+
+struct ZipCode: RawRepresentable {
+    let rawValue: String
+
+    init?(rawValue: String) {
+        guard rawValue.characters.count == 5,
+            CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: rawValue))
+            else {
+                return nil
+        }
+
+        self.rawValue = rawValue
+    }
+}

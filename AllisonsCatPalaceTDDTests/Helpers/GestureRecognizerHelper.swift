908bcaa91b8b5255ce5175dc8c7b2b86ec213c82
joesus <joesusnick@gmail.com>
2017-11-23 13:14:32 -0600

    Actually adds the gesture recognizer, not just an empty file.

--- AllisonsCatPalaceTDDTests/Helpers/GestureRecognizerHelper.swift
+++ AllisonsCatPalaceTDDTests/Helpers/GestureRecognizerHelper.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+import UIKit
+import Foundation
+
+extension UIGestureRecognizer {
+
+    var targetedActions: [(target: Any, action: Selector)] {
+        guard let targetActionPairs = value(forKey: "_targets") as? [AnyObject] else {
+            fatalError("Unable to extract target/action references from gesture recognizer type")
+        }
+
+        return targetActionPairs.flatMap { pair in
+            guard let target = pair.value(forKey: "_target"),
+                let action = pair.description?.extractActionSelector()
+                else {
+                    return nil
+            }
+
+            return (target: target, action: action)
+        }
+    }
+
+}
+
+private extension String {
+
+    func extractActionSelector() -> Selector? {
+        guard let selectorString = trimmingCharacters(in: CharacterSet(charactersIn: "()"))
+            .components(separatedBy: ", ")
+            .map({ $0.components(separatedBy: "=") })
+            .filter({ $0.first ?? "" == "action" })
+            .first?
+            .dropFirst()
+            .first
+            else {
+                return nil
+        }
+
+        return NSSelectorFromString(selectorString)
+    }
+
+}

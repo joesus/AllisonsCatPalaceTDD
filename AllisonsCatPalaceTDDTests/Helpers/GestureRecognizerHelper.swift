707c5e7ab9021ee17cea96fa9cf400679e48de0f
joesus <joesusnick@gmail.com>
2017-11-23 13:11:32 -0600

    Adds helpers for testing segues and gesture recognizers

---
+++ AllisonsCatPalaceTDDTests/Helpers/GestureRecognizerHelper.swift
@@ -0,0 +1,9 @@
+//
+//  GestureRecognizerHelper.swift
+//  AllisonsCatPalaceTDDTests
+//
+//  Created by Joe Susnick on 11/23/17.
+//  Copyright © 2017 Joesus. All rights reserved.
+//
+
+import Foundation
---
+++ AllisonsCatPalaceTDDTests/Helpers/SegueTemplates.swift
@@ -0,0 +1,60 @@
+import UIKit
+
+class SegueTemplate: NSObject {
+
+    let rawTemplate: AnyObject
+
+    init(hiddenTemplate: AnyObject) {
+        rawTemplate = hiddenTemplate
+
+        super.init()
+    }
+
+    private static let storyboardSegueTemplateIdentifierKey = "identifier"
+
+    var identifier: String {
+        return rawTemplate.value(
+            forKey: SegueTemplate.storyboardSegueTemplateIdentifierKey
+            ) as! String
+    }
+
+    private static let storyboardSegueTemplateDestinationStringKey
+        = "_destinationViewControllerIdentifier"
+
+    var destinationSceneIdentifier: String {
+        return rawTemplate.value(
+            forKey: SegueTemplate.storyboardSegueTemplateDestinationStringKey
+            ) as! String
+    }
+
+}
+
+extension UIViewController {
+
+    func segueTemplate(identifiedBy segueIdentifier: String) -> SegueTemplate? {
+        return segueTemplates?.first { $0.identifier == segueIdentifier }
+    }
+
+    private static let storyboardSegueTemplatesKey = "storyboardSegueTemplates"
+
+    private var segueTemplates: [SegueTemplate]? {
+        let rawTemplates = value(forKey: UIViewController.storyboardSegueTemplatesKey)
+            as? [AnyObject]
+        return rawTemplates?.map(SegueTemplate.init(hiddenTemplate:))
+    }
+
+}
+
+extension UITableViewCell {
+
+    private static let selectionSegueTemplateKey = "_selectionSegueTemplate"
+
+    var selectionSegueTemplate: SegueTemplate? {
+        guard let rawTemplate = value(forKey: UITableViewCell.selectionSegueTemplateKey) else {
+            return nil
+        }
+
+        return SegueTemplate(hiddenTemplate: rawTemplate as AnyObject)
+    }
+
+}

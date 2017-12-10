f6f2aebdac8a2e7f50d8c5c9e88d676eb4353aeb
joesus <joesusnick@gmail.com>
2017-12-10 14:12:07 -0700

    Adds sample data for placemark and error

---
+++ AllisonsCatPalaceTDDTests/SampleData/SamplePlacemark.swift
@@ -0,0 +1,23 @@
+//
+//  SamplePlacemark.swift
+//  AllisonsCatPalaceTDDTests
+//
+//  Created by Joe Susnick on 12/10/17.
+//  Copyright © 2017 Joesus. All rights reserved.
+//
+
+import CoreLocation
+
+enum SamplePlacemark {
+    static let denver: CLPlacemark = {
+        guard let url = Bundle(for: UserLocationResolutionTests.self)
+            .url(forResource: "EncodedPlacemark", withExtension: nil),
+            let placemark = NSKeyedUnarchiver.unarchiveObject(withFile: url.path)
+                as? CLPlacemark
+            else {
+                fatalError()
+        }
+
+        return placemark
+    }()
+}

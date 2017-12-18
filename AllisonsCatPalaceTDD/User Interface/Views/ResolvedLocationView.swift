f3e64cb792eebef4a66687c3807ad90b8a5835a0
joesus <joesusnick@gmail.com>
2017-12-17 17:05:03 -0700

    resolved location view does not rely on xib, is able to format and display location information

--- AllisonsCatPalaceTDD/User Interface/Views/ResolvedLocationView.swift
+++ AllisonsCatPalaceTDD/User Interface/Views/ResolvedLocationView.swift
@@ -8,36 +8,73 @@
 
 import UIKit
 
-@IBDesignable
 class ResolvedLocationView: UIView {
 
-    @IBOutlet weak var locationIndicator: UIImageView!
-    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var label: UILabel!
 
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    enum SimplifiedLocationName {
+        case unknown
+        case withZipCode(code: String, cityOrState: String?)
+        case withoutZipCode(cityOrState: String, state: String?)
 
-        loadNib()
-    }
+        init(city: String?, state: String?, zip: String?) {
+            if let validCity = city {
+                if let validState = state {
+                    self = .withoutZipCode(cityOrState: validCity, state: validState)
+                }
+                else if let validZip = zip {
+                    self = .withZipCode(code: validZip, cityOrState: validCity)
+                }
+                else {
+                    self = .withoutZipCode(cityOrState: validCity, state: nil)
+                }
+            }
+            else if let validState = state {
+                if let validZip = zip {
+                    self = .withZipCode(code: validZip, cityOrState: validState)
+                }
+                else {
+                    self = .withoutZipCode(cityOrState: validState, state: nil)
+                }
+            }
+            else if let validZip = zip {
+                self = .withZipCode(code: validZip, cityOrState: nil)
+            }
+            else {
+                self = .unknown
+            }
+        }
 
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+        var displayableString: String {
+            switch self {
+            case .unknown:
+                return "Location Unknown"
 
-        loadNib()
-    }
+            case .withZipCode(let code, let cityOrState):
+                var secondaryString: String?
+                if let secondaryLocation = cityOrState {
+                    secondaryString = "(\(secondaryLocation))"
+                }
 
-    private func loadNib() {
-        let bundle = Bundle(for: ResolvedLocationView.self)
-        if let view = bundle.loadNibNamed("ResolvedLocationView", owner: self)?.first as? UIView {
-            view.frame = bounds
-            addSubview(view)
+                let strings = [code, secondaryString]
+                return strings.flatMap { $0 }
+                    .joined(separator: " ")
+
+            case .withoutZipCode(let cityOrState, let state):
+                return [cityOrState, state].flatMap { $0 }
+                    .joined(separator: ", ")
+            }
         }
     }
 
-    override func prepareForInterfaceBuilder() {
-        super.prepareForInterfaceBuilder()
+    func configure(city: String? = nil, state: String? = nil, zip: String? = nil) {
+        let locationName = SimplifiedLocationName(
+            city: city,
+            state: state,
+            zip: zip
+        )
 
-        loadNib()
+        label.text = locationName.displayableString
     }
 }
-
--- AllisonsCatPalaceTDDTests/ResolvedLocationViewTests.swift
+++ AllisonsCatPalaceTDDTests/ResolvedLocationViewTests.swift
@@ -12,59 +12,51 @@ import XCTest
 
 class ResolvedLocationViewTests: XCTestCase {
 
-    func testContainsAView() {
-        let bundle = Bundle(for: ResolvedLocationView.self)
-        guard let _ = bundle.loadNibNamed("ResolvedLocationView", owner: ResolvedLocationView().self)?.first as? UIView else {
-            return XCTFail("Resolved location view did not contain a UIView")
+    let view = ResolvedLocationView()
+    let imageView = UIImageView()
+    let label = UILabel()
+
+
+    override func setUp() {
+        super.setUp()
+
+        view.icon = imageView
+        view.label = label
+    }
+
+    func testConfiguringWithLocationData() {
+        typealias DisplayableLocation = (
+            city: String?,
+            state: String?,
+            zip: String?
+        )
+        typealias DisplayedLocation = (location: DisplayableLocation, displayedValue: String)
+
+        let city = "Denver".displayableString!
+        let state = "Colorado".displayableString!
+        let zip = "80220".displayableString!
+
+        let testValues: [DisplayedLocation] = [
+            ((city, nil, nil), city),
+            ((city, state, zip), "\(city), \(state)"),
+            ((city, state, nil), "\(city), \(state)"),
+            ((city, nil, zip), "\(zip) (\(city))"),
+            ((nil, state, zip), "\(zip) (\(state))"),
+            ((nil, state, nil), state),
+            ((nil, nil, zip), zip),
+            ((nil, nil, nil), "Location Unknown")
+        ]
+
+        testValues.forEach { value in
+            view.configure(
+                city: value.location.city,
+                state: value.location.state,
+                zip: value.location.zip
+            )
+
+            XCTAssertEqual(label.text, value.displayedValue,
+                           "Resolved location view's label should have different value depending on the input")
         }
-    }
-
-    func testInitializingWithFrame() {
-        let frame = CGRect(x: 0, y: 0, width: 10, height: 10)
-        XCTAssertFalse(ResolvedLocationView(frame: frame).subviews.isEmpty,
-                       "Resolved location view should add subviews from nib when instantiated in code")
-    }
-
-    func testIsIBDesignable() {
-        let view = ResolvedLocationView()
-        view.subviews.forEach { $0.removeFromSuperview() }
-        XCTAssertEqual(view.subviews, [],
-                       "Resolved location view should have all subviews removed")
-        view.prepareForInterfaceBuilder()
-        XCTAssertFalse(view.subviews.isEmpty,
-                       "Resolved location view should add subviews from nib when prepared for interface builder")
-    }
-
-    func testHasLocationIndicator() {
-        let frame = CGRect(x: 0, y: 0, width: 10, height: 10)
-        let view = ResolvedLocationView(frame: frame)
-        guard let locationIndicator = view.locationIndicator else {
-            return XCTFail("Resolved location view should contain a location indicator")
-        }
-
-        XCTAssertEqual(locationIndicator.image, #imageLiteral(resourceName: "location-pin"),
-                       "Resolved location view should have an indicator image of a pin")
-    }
-
-    func testHasLocationLabel() {
-        let frame = CGRect(x: 0, y: 0, width: 10, height: 10)
-        let view = ResolvedLocationView(frame: frame)
-
-        guard let label = view.locationLabel else {
-            return XCTFail("Resolved location view should contain a location label")
-        }
-
-        XCTAssertEqual(label.text, "Location",
-                       "Resolved location view's label should have the expected text")
-        XCTAssertEqual(label.font, UIFont.preferredFont(forTextStyle: .title2),
-                       "Resolved location view's label should have a title2 text style so that it can display larger sizes when needed")
-        XCTAssertEqual(label.numberOfLines, 0,
-                       "Resolved location view's label should have number of lines set to zero so that it display long text on multiple lines.")
-        XCTAssertEqual(label.lineBreakMode, .byWordWrapping,
-                       "Resolved location view's label should have line break mode set to word wrap so that it can display long text on multiple lines.")
-        XCTAssertFalse(label.isHidden,
-                       "Resolved location view's label should not be hidden")
     }
 
 }
-

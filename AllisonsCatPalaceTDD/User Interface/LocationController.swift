9dce854d45043743059e15c614e598778d27c60c
joesus <joesusnick@gmail.com>
2018-01-06 16:22:46 -0700

    adds location resolution user interface

--- AllisonsCatPalaceTDD/User Interface/LocationController.swift
+++ AllisonsCatPalaceTDD/User Interface/LocationController.swift
@@ -11,11 +11,46 @@ import CoreLocation
 
 class LocationController: UIViewController {
 
+    private(set) var userLocationResolution: UserLocationResolution = {
+        guard CLLocationManager.locationServicesEnabled() else {
+            return .disallowed
+        }
+
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            return .unknown
+        case .authorizedWhenInUse, .authorizedAlways:
+            return .allowed
+        case .denied, .restricted:
+            return .disallowed
+        }
+    }()
+
+    @IBOutlet weak var locationResolutionStack: UIStackView!
+    @IBOutlet weak var resolvingLocationView: ResolvingLocationView!
+    @IBOutlet weak var resolvedLocationView: ResolvedLocationView!
+
+    @IBOutlet weak var actionableMessageView: UIView!
+    @IBOutlet weak var actionableMessageLabel: UILabel!
+    @IBOutlet weak var actionableMessageButton: UIButton!
+
+    @IBOutlet weak var searchButton: UIBarButtonItem!
+    @IBOutlet var favoritesButton: UIBarButtonItem!
+    @IBOutlet weak var speciesSelectionControl: UISegmentedControl! {
+        didSet {
+            speciesSelectionControl.setTitleTextAttributes(
+                [NSFontAttributeName: UIFont.preferredFont(forTextStyle: .title2)],
+                for: .normal
+            )
+        }
+    }
+
     var geocoder = CLGeocoder()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureLocationResolutionStack()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -43,13 +78,74 @@ class LocationController: UIViewController {
 
     }
 
+    func transition(to locationResolution: UserLocationResolution) {
+        userLocationResolution = locationResolution
+        configureLocationResolutionStack()
+    }
 
+    func configureLocationResolutionStack() {
+        // hide all
+        locationResolutionStack.arrangedSubviews.forEach { subview in
+            subview.isHidden = true
         }
 
+        var viewToShow: UIView?
+        switch userLocationResolution {
+        case .disallowed:
+            viewToShow = actionableMessageView
+            configureActionableMessageView()
+
+        case .resolving:
+            viewToShow = resolvingLocationView
+
+        case .resolved:
+            viewToShow = resolvedLocationView
+            configureResolvedLocationView()
+
+        case .resolutionFailure:
+            viewToShow = actionableMessageView
+            configureActionableMessageView()
+
+        default:
+            break
         }
 
+        viewToShow?.isHidden = false
+    }
+
+    private func configureActionableMessageView() {
+        let message: String
+        let title: String
+
+        switch userLocationResolution {
+        case .disallowed:
+            message = "Looks like you're in a top secret location. In order to find the pets closest to you, AdoptR needs to know where you are."
+            title = "Open Settings"
+
+        case .resolutionFailure:
+            message = "We seem to be having trouble locating you."
+            title = "Find My Location"
+
+        default:
+            return
+        }
+
+        actionableMessageLabel.text = message
+        actionableMessageButton.setTitle(title, for: .normal)
+    }
+
+    private func configureResolvedLocationView() {
+        guard case .resolved(let location) = userLocationResolution else { return }
+
+        resolvedLocationView.configure(
+            city: location.locality,
+            state: location.administrativeArea,
+            zip: location.postalCode
+        )
+    }
 
             return false
     }
 
+    @IBAction func continueLocationResolution() {}
 }
--- AllisonsCatPalaceTDDTests/LocationControllerTests.swift
+++ AllisonsCatPalaceTDDTests/LocationControllerTests.swift
@@ -121,7 +121,7 @@ class LocationControllerTests: XCTestCase {
 
     func testViewDidLoad() {
         UIViewController.ViewDidLoadSpyController.createSpy(on: controller)!.spy {
-            controller.viewDidLoad()
+            loadComponents()
             XCTAssert(controller.superclassViewDidLoadCalled, "ViewDidLoad should call viewDidLoad on UIViewController")
         }
     }
@@ -135,12 +135,275 @@ class LocationControllerTests: XCTestCase {
         }
     }
 
-    }
+    func testSceneHasUserResolutionIndicationElementsInAStack() {
+        loadComponents()
 
+        guard let stackView = controller.locationResolutionStack else {
+            return XCTFail("Controller should have a stack to hold the location resolution indicators")
         }
 
+        let expectedStackContents: [UIView] = [
+            controller.resolvingLocationView!,
+            controller.resolvedLocationView!,
+            controller.actionableMessageView!
+        ]
+
+        XCTAssertEqual(stackView.arrangedSubviews, expectedStackContents,
+                       "Controller's location resolution indicators should be enclosed in a stack in the proper order")
     }
 
+    func testResolvingLocationView() {
+        loadComponents()
+
+        guard let view = controller.resolvingLocationView,
+            let activityIndicator = view.activityIndicator,
+            let label = view.label
+            else {
+                return XCTFail("Controller should have a view with an activity indicator and label for indicating that location is being resolved")
+        }
+
+        XCTAssertTrue(view.isHidden,
+                      "Resolving location view should be hidden by default")
+        XCTAssertEqual(label.text, "Finding Location",
+                       "Resolving location view's label should have the expected text")
+        XCTAssertEqual(label.font, UIFont.systemFont(ofSize: 24),
+                       "Resolving location view's label should have 24 point font")
+        XCTAssertEqual(label.numberOfLines, 1,
+                       "Resolving location view's label should have number of lines set to one")
+        XCTAssertEqual(label.lineBreakMode, .byTruncatingTail,
+                       "Resolving location view's label should have line break mode set to truncate tail")
+        XCTAssertFalse(label.isHidden,
+                       "Resolving location view's label should not be hidden")
+    }
+
+    func testResolvedLocationView() {
+        loadComponents()
+
+        guard let view = controller.resolvedLocationView,
+            let icon = view.icon,
+            let label = view.label
+            else {
+                return XCTFail("Controller should have a view with an icon and label for indicating that location has been resolved")
+        }
+
+        XCTAssertTrue(view.isHidden,
+                      "Resolved location view should be hidden by default")
+        XCTAssertEqual(icon.image, #imageLiteral(resourceName: "location-pin"),
+                       "Resolved location view should have an indicator image of a pin")
+
+        XCTAssertEqual(label.font, .systemFont(ofSize: 24),
+                       "Resolved location view's label should have 24 point font")
+        XCTAssertEqual(label.numberOfLines, 1,
+                       "Resolved location view's label should have number of lines set to one")
+        XCTAssertEqual(label.lineBreakMode, .byTruncatingTail,
+                       "Resolved location view's label should have line break mode set to truncate tail")
+        XCTAssertFalse(label.isHidden,
+                       "Resolved location view's label should not be hidden")
+    }
+
+    func testActionableMessageView() {
+        loadComponents()
+
+        guard let view = controller.actionableMessageView,
+            let label = controller.actionableMessageLabel,
+            label.superview === view,
+            let button = controller.actionableMessageButton,
+            button.superview === view
+            else {
+                return XCTFail("Controller should have an actionable message view with a label and a button")
+        }
+
+        XCTAssertTrue(view.isHidden,
+                      "Actionable message view should be hidden by default")
+        XCTAssertEqual(label.font, UIFont.preferredFont(forTextStyle: .body),
+                       "The message should have a body text style so that it can display larger sizes when needed")
+        XCTAssertEqual(label.numberOfLines, 0,
+                       "The message should have number of lines set to zero so that it display long text on multiple lines.")
+        XCTAssertEqual(label.lineBreakMode, .byWordWrapping,
+                       "The message should have line break mode set to word wrap so that it can display long text on multiple lines.")
+        XCTAssertFalse(button.isHidden,
+                       "The message should not be hidden")
+
+        guard let action = button.actions(
+            forTarget: controller,
+            forControlEvent: .touchUpInside
+            )?.first
+            else {
+                return XCTFail("The button should have an associated action")
+        }
+
+        XCTAssertEqual(action, #selector(LocationController.continueLocationResolution).description,
+                       "The button should notify the view of the user's intent to find location")
+
+    }
+
+    func testSpeciesSelectionControl() {
+        loadComponents()
+
+        guard let control = controller.speciesSelectionControl else {
+            return XCTFail("Controller should have a species selection control")
+        }
+
+        XCTAssertEqual(control.numberOfSegments, 3,
+                       "Species selection control should have three options")
+        XCTAssertEqual(control.selectedSegmentIndex, SpeciesSelectionIndex.cat.rawValue,
+                       "Cat segment should be selected by default")
+
+        let indices = [SpeciesSelectionIndex.cat, .dog, .any]
+        indices.forEach { index in
+            guard let title = control.titleForSegment(at: index.rawValue) else {
+                return XCTFail("Each segment should have a title")
+            }
+
+            let expectedTitle: String
+            switch index {
+            case .cat: expectedTitle = "Cat"
+            case .dog: expectedTitle = "Dog"
+            case .any: expectedTitle = "Any"
+            }
+
+            XCTAssertEqual(title, expectedTitle,
+                           "Title for segment at \(index.rawValue) should be \(index)")
+        }
+
+        guard let attributes = control.titleTextAttributes(for: .normal),
+            let font = attributes[NSFontAttributeName] as? UIFont
+            else {
+                return XCTFail("Species segmented control should have a known font")
+        }
+
+        XCTAssertEqual(font, UIFont.preferredFont(forTextStyle: .title2),
+                       "Species segmented control should have a custom font for its normal state")
+    }
+
+    func testSearchButton() {
+        loadComponents()
+
+        guard let button = searchButton,
+            let target = button.target
+            else {
+                return XCTFail("Navigation bar should have a right bar button item with a target")
+        }
+
+        XCTAssertEqual(button.title, "Search",
+                       "Search button should exist and have correct title")
+        XCTAssertFalse(button.isEnabled,
+                       "Search button should not be enabled by default")
+
+        XCTAssertTrue(
+            target.isKind(of: NSClassFromString("UIStoryboardShowSegueTemplate")!),
+            "Search button target should be a show segue template"
+        )
+
+        guard let template = controller.segueTemplate(identifiedBy: "ShowSearch"),
+            template.destinationSceneIdentifier == "SearchResultsScene"
+            else {
+                return XCTFail("The controller should have a segue to transition to the search results scene")
+        }
+
+        XCTAssertTrue(target === template.rawTemplate,
+                      "The buttons target should be a show segue")
+    }
+
+    func testViewConfigurationForUnknownLocationResolution() {
+        loadComponents()
+        controller.transition(to: .unknown)
+
+        XCTAssertTrue(controller.resolvingLocationView.isHidden,
+                      "Resolving location view should be hidden with unknown resolution")
+        XCTAssertTrue(controller.resolvedLocationView.isHidden,
+                      "Resolved location view should be hidden with unknown resolution")
+        XCTAssertTrue(controller.actionableMessageView.isHidden,
+                      "Actionable message view should be hidden with unknown resolution")
+    }
+
+    func testViewConfigurationForAllowedLocationResolution() {
+        loadComponents()
+        controller.transition(to: .allowed)
+
+        XCTAssertTrue(controller.resolvingLocationView.isHidden,
+                      "Resolving location view should be hidden with allowed resolution")
+        XCTAssertTrue(controller.resolvedLocationView.isHidden,
+                      "Resolved location view should be hidden with allowed resolution")
+        XCTAssertTrue(controller.actionableMessageView.isHidden,
+                      "Actionable message view should be hidden with allowed resolution")
+    }
+
+    func testViewConfigurationForDisallowedLocationResolution() {
+        loadComponents()
+        controller.transition(to: .disallowed)
+
+        XCTAssertTrue(controller.resolvingLocationView.isHidden,
+                      "Resolving location view should be hidden with disallowed resolution")
+        XCTAssertTrue(controller.resolvedLocationView.isHidden,
+                      "Resolved location view should be hidden with disallowed resolution")
+        XCTAssertFalse(controller.actionableMessageView.isHidden,
+                       "Actionable message view should not be hidden with disallowed resolution")
+
+        guard let label = controller.actionableMessageLabel,
+            let button = controller.actionableMessageButton
+            else {
+                return XCTFail("Controller should have an actionable message view with a label and a button")
+        }
+
+        XCTAssertEqual(
+            label.text,
+            "Looks like you're in a top secret location. In order to find the pets closest to you, AdoptR needs to know where you are.",
+            "Actionable message label should be configured correctly for disallowed resolution"
+        )
+        XCTAssertEqual(button.title(for: .normal), "Open Settings",
+                       "Actionable message button should be configured correctly for disallowed resolution")
+    }
+
+    func testViewConfigurationForInProgressLocationResolution() {
+        loadComponents()
+        controller.transition(to: .resolving)
+
+        XCTAssertFalse(controller.resolvingLocationView.isHidden,
+                      "Resolving location view should not be hidden with resolving resolution")
+        XCTAssertTrue(controller.resolvedLocationView.isHidden,
+                      "Resolved location view should be hidden with resolving resolution")
+        XCTAssertTrue(controller.actionableMessageView.isHidden,
+                       "Actionable message view should be hidden with resolving resolution")
+    }
+
+    func testViewConfigurationForSuccessfulLocationResolution() {
+        loadComponents()
+        controller.transition(to: .resolved(location: SamplePlacemarks.denver))
+
+        XCTAssertTrue(controller.resolvingLocationView.isHidden,
+                      "Resolving location view should be hidden with a resolved location")
+        XCTAssertFalse(controller.resolvedLocationView.isHidden,
+                      "Resolved location view should not be hidden with a resolved location")
+        XCTAssertTrue(controller.actionableMessageView.isHidden,
+                      "Actionable message view should be hidden with a resolved location")
+
+        XCTAssertEqual(controller.resolvedLocationView.label.text, "Denver, CO",
+                       "Resolving location label should be configured correctly for resolving resolution")
+    }
+
+    func testViewConfigurationForFailedLocationResolution() {
+        loadComponents()
+        controller.transition(to: .resolutionFailure(error: GeocodingError.unknownError))
+
+        XCTAssertTrue(controller.resolvingLocationView.isHidden,
+                      "Resolving location view should be hidden with resolution failure")
+        XCTAssertTrue(controller.resolvedLocationView.isHidden,
+                      "Resolved location view should be hidden with resolution failure")
+        XCTAssertFalse(controller.actionableMessageView.isHidden,
+                       "Actionable message view should not be hidden with resolution failure")
+
+        XCTAssertEqual(
+            controller.actionableMessageLabel.text,
+            "We seem to be having trouble locating you.",
+            "Actionable message label should be configured correctly for resolution failure"
+        )
+        XCTAssertEqual(
+            controller.actionableMessageButton.title(for: .normal),
+            "Find My Location",
+            "Actionable message button should be configured correctly for resolution failure"
+        )
+    }
 
 
 

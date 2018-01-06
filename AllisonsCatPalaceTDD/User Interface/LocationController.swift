cf6ab8a05961182c85d7e9a21d97369507c93f30
joesus <joesusnick@gmail.com>
2018-01-06 16:16:23 -0700

    Removes textfield from location controller.

--- AllisonsCatPalaceTDD/User Interface/LocationController.swift
+++ AllisonsCatPalaceTDD/User Interface/LocationController.swift
@@ -10,29 +10,22 @@ import UIKit
 import CoreLocation
 
 class LocationController: UIViewController {
-    @IBOutlet weak var zipCodeField: UITextField!
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
     var geocoder = CLGeocoder()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        decorateZipCodeField()
-        populateZipCodeField()
-        zipCodeField.addTarget(self, action: #selector(LocationController.textFieldDidChange(_:)), for: .valueChanged)
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        populateZipCodeField()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
-        zipCodeField.becomeFirstResponder()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
@@ -43,93 +36,20 @@ class LocationController: UIViewController {
         }
     }
 
-    fileprivate func decorateZipCodeField() {
-        zipCodeField.layer.cornerRadius = 4
-        zipCodeField.layer.borderWidth = 1
-        zipCodeField.layer.borderColor = UIColor.lightGray.cgColor
-    }
 
-    fileprivate func populateZipCodeField() {
-        let storedZipCode = SettingsManager.shared.value(forKey: .zipCode) as? String
-        zipCodeField.text = storedZipCode
-    }
-
-    fileprivate var zipCodeText: String {
-        return zipCodeField.text ?? ""
-    }
-
-    func textFieldDidChange(_ textField: UITextField) {
-        if zipCodeText.characters.count >= 5 {
-            textField.endEditing(true)
-        }
-    }
-}
-
-extension LocationController: UITextFieldDelegate {
-
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        textField.layer.borderColor = UIColor.lightGray.cgColor
-
-        activityIndicator.stopAnimating()
-    }
-
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let originalString = textField.text ?? ""
-        let newString = (originalString as NSString).replacingCharacters(in: range, with: string)
-        let newStringOnlyContainsDecimals = CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: newString))
-
-        guard newStringOnlyContainsDecimals else {
-            return false
         }
 
-        if newString.characters.count == 5 {
-            textField.resignFirstResponder()
+
+
+    }
+
+
         }
 
-        if newString.characters.count > 5 {
+        }
+
 
             return false
-        }
-
-        return true
     }
 
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        activityIndicator.startAnimating()
-        textField.isUserInteractionEnabled = false
-
-        geocodeAddress()
-    }
-
-    private func geocodeAddress() {
-        guard ZipCode(rawValue: zipCodeText) != nil else {
-            return
-        }
-
-        geocoder.geocodeAddressString(zipCodeText) { [weak self] (placemarks, error) in
-            // geocode
-            self?.activityIndicator.stopAnimating()
-            self?.zipCodeField.isUserInteractionEnabled = true
-
-            guard error == nil,
-                let locations = placemarks,
-                locations.isEmpty == false,
-                let zipCode = locations.first?.postalCode else {
-
-                    self?.zipCodeField.layer.borderColor = UIColor.red.cgColor
-                    return
-            }
-
-            SettingsManager.shared.set(value: zipCode, forKey: .zipCode)
-
-            //Do not perform a segue if it's not the top view controller - probably means the user hit the back button which would also end editing,
-            // The task should have been cancelled by this point from viewWillDisappear but just in case
-            guard let navController = self?.navigationController,
-                navController.topViewController == self else {
-                return
-            }
-
-            self?.performSegue(withIdentifier: "ShowAnimalCardsViewController", sender: nil)
-        }
-    }
 }
--- AllisonsCatPalaceTDDTests/LocationControllerTests.swift
+++ AllisonsCatPalaceTDDTests/LocationControllerTests.swift
@@ -15,7 +15,6 @@ import XCTest
 
 class LocationControllerTests: XCTestCase {
     var controller: LocationController!
-    var textField: UITextField!
     var delegate: UITextFieldDelegate!
     var geocoder: CLGeocoder!
     var geocoderSpy: Spy?
@@ -31,7 +30,6 @@ class LocationControllerTests: XCTestCase {
     override func setUp() {
         super.setUp()
 
-        SettingsManager.shared.clear() // Clears persisted zip code
 
         CLLocationManager.beginStubbingLocationServicesEnabled(with: true)
         CLLocationManager.beginStubbingAuthorizationStatus(with: .authorizedWhenInUse)
@@ -137,109 +135,36 @@ class LocationControllerTests: XCTestCase {
         }
     }
 
-    func testBecomesFirstResponderOnAppearing() {
-        replaceRootViewController(with: controller)
-        controller.viewDidAppear(false)
-        XCTAssertTrue(textField.isFirstResponder,
-                      "Textfield should be first responder on view did appear")
     }
 
-    func testZipCodeField() {
-        guard let field = textField else {
-            return XCTFail("Location controller should have a zip code field")
         }
 
-        verifyTextFieldIsEmpty(field,
-                               "ZipCode field should be empty by default")
-        XCTAssertEqual(field.borderStyle, .roundedRect,
-                       "Zip code field should have rounded border")
-        XCTAssertEqual(field.keyboardType, .numberPad,
-                       "Zip code field should only accept numeric input")
-        XCTAssertEqual(field.layer.cornerRadius, 4,
-                       "Zip code field's corner radius should be four points")
-        XCTAssertEqual(field.layer.borderWidth, 1,
-                       "Zip code field's border width should be one point")
-        XCTAssertEqual(field.layer.borderColor, UIColor.lightGray.cgColor,
-                       "Zip code field's border color should be light gray")
-        XCTAssertTrue(field.delegate === controller,
-                      "Zip code field's delegate should be the controller")
     }
 
-    func testZipCodeFieldNumbersOnly() {
-        XCTAssertTrue(delegate.textField!(textField, shouldChangeCharactersIn: NSRange(), replacementString: "0"),
-                      "Zip code field should accept numeric input")
 
-        XCTAssertFalse(delegate.textField!(textField, shouldChangeCharactersIn: NSRange(), replacementString: "a"),
-                       "Zip code field should not accept alphabetic input")
 
-        XCTAssertFalse(delegate.textField!(textField, shouldChangeCharactersIn: NSRange(), replacementString: "!"),
-                       "Zip code field should not accept special character input")
 
-        XCTAssertFalse(delegate.textField!(textField, shouldChangeCharactersIn: NSRange(), replacementString: "@"),
-                       "Zip code field should not accept special character input")
 
-        XCTAssertFalse(delegate.textField!(textField, shouldChangeCharactersIn: NSRange(), replacementString: "."),
-                       "Zip code field should not accept special character input")
 
-        XCTAssertFalse(delegate.textField!(textField, shouldChangeCharactersIn: NSRange(), replacementString: " "),
-                       "Zip code field should not accept white space input")
 
-        XCTAssertFalse(delegate.textField!(textField, shouldChangeCharactersIn: NSRange(), replacementString: "\n"),
-                       "Zip code field should not accept newline input")
     }
 
-    func testZipCodeFieldCharacterLimit() {
-        textField.text = ""
-        XCTAssertTrue(delegate.textField!(textField, shouldChangeCharactersIn: NSRange(), replacementString: "8"))
 
-        textField.text = "8"
-        XCTAssertTrue(delegate.textField!(textField, shouldChangeCharactersIn: NSRange(location: 1, length: 0), replacementString: "0"))
 
-        textField.text = "80"
-        XCTAssertTrue(delegate.textField!(textField, shouldChangeCharactersIn: NSRange(location: 2, length: 0), replacementString: "2"))
 
-        textField.text = "802"
-        XCTAssertTrue(delegate.textField!(textField, shouldChangeCharactersIn: NSRange(location: 3, length: 0), replacementString: "2"))
 
-        textField.text = "8022"
-        XCTAssertTrue(delegate.textField!(textField, shouldChangeCharactersIn: NSRange(location: 4, length: 0), replacementString: "0"))
 
-        textField.text = "80220"
-        XCTAssertFalse(delegate.textField!(textField, shouldChangeCharactersIn: NSRange(location: 5, length: 0), replacementString: "8"))
     }
 
-    func testZipCodeFieldBeginsEditing() {
-        // resets borderColor
-        textField.layer.borderColor = UIColor.green.cgColor
-        XCTAssertEqual(textField.layer.borderColor, UIColor.green.cgColor,
-                       "Zip code field's border color should be green")
 
-        delegate.textFieldDidBeginEditing?(textField)
-        XCTAssertEqual(textField.layer.borderColor, UIColor.lightGray.cgColor,
-                       "Zip code field's border color should be reset to light gray on beginning text editing")
 
-        // stops spinner
-        controller.activityIndicator.startAnimating()
-        delegate.textFieldDidBeginEditing?(textField)
-        XCTAssertFalse(controller.activityIndicator.isAnimating,
-                       "Activity indicator should stop animating on beginning text editing")
     }
 
-    func testZipCodeFieldEndsEditingAtFiveCharacters() {
-        UITextField.ResignFirstResponderSpyController.createSpy(on: textField)?.spy {
-            // Need to be in the window to becomeFirstResponder
-            replaceRootViewController(with: controller)
-            textField.becomeFirstResponder()
 
-            textField.text = "80220"
-            controller.textFieldDidChange(textField)
 
-            XCTAssertTrue(textField.resignFirstResponderCalled,
-                          "Textfield should resign first responder at five characters")
         }
     }
 
-    func testGeocoderIsNotCalledWithoutValidZipCode() {
         replaceRootViewController(with: controller)
         attemptGeocoding(withText: "000")
 
@@ -247,45 +172,10 @@ class LocationControllerTests: XCTestCase {
                        "Geocoder should not be called with an invalid zip code")
     }
 
-    func testZipCodeFieldGeocodesAtFiveCharacters() {
-        replaceRootViewController(with: controller)
-        attemptGeocoding(withText: "80220")
-
-        XCTAssertTrue(geocoder.forwardGeocodeAddressCalled,
-                      "Textfield should forward geocode when it ends editing")
-    }
-
-    func testActivityIndicatorHiddenByDefault() {
-        XCTAssertTrue(controller.activityIndicator.isHidden,
-                      "Activity indicator should be hidden by default")
-        XCTAssertTrue(controller.activityIndicator.hidesWhenStopped,
-                      "Activity indicator should hide when stopped")
-    }
-
     func testGeocodingInProgress() {
-        replaceRootViewController(with: controller)
-        attemptGeocoding(withText: "80220")
-
-        XCTAssertTrue(controller.activityIndicator.isAnimating,
-                      "Activity indicator should animate while geocoding")
-
-        XCTAssertFalse(textField.isUserInteractionEnabled,
-                       "User interaction should be disabled when geocoding is in progress")
     }
 
     func testGeocodingCompleted() {
-        replaceRootViewController(with: controller)
-        attemptGeocoding(withText: "80220")
-
-        guard let handler = geocoder.forwardGeocodeAddressCompletionHandler else {
-            return XCTFail("Geocoder should be called with a handler")
-        }
-        handler([], nil)
-
-        XCTAssertFalse(controller.activityIndicator.isAnimating,
-                       "Activity indicator should not be animating when geocoding is not in progress")
-        XCTAssertTrue(textField.isUserInteractionEnabled,
-                      "User interaction should be enabled when geocoding is not in progress")
     }
 
     func testGeocodingWithEmptyResultsAndError() {
@@ -294,9 +184,6 @@ class LocationControllerTests: XCTestCase {
 
         geocoder.forwardGeocodeAddressCompletionHandler?([], GeocodingError.noLocationsFound)
 
-        XCTAssertEqual(textField.layer.borderColor, UIColor.red.cgColor,
-                       "Zip code field should have red border on error")
-        // TODO: maybe show an error message depending on the type of error that comes back?
     }
 
     func testGeocodingWithEmptyResultsAndNoError() {
@@ -308,9 +195,6 @@ class LocationControllerTests: XCTestCase {
         }
         handler([], nil)
 
-        XCTAssertEqual(textField.layer.borderColor, UIColor.red.cgColor,
-                       "Zip code field should have red border when there are no results")
-        // TODO: use custom error for empty results to show the message
     }
 
     func testGeocodingWithNonEmptyResultsAndNoError() {
@@ -322,8 +206,6 @@ class LocationControllerTests: XCTestCase {
         }
         handler([placemark], nil)
 
-        XCTAssertEqual(textField.layer.borderColor, UIColor.lightGray.cgColor,
-                       "Zip code field should have non-error color border when there are results and no error")
     }
 
     func testViewWillDisappearCancelsGeocoding() {
@@ -338,110 +220,13 @@ class LocationControllerTests: XCTestCase {
 //        }
     }
 
-    func testSuccessfulGeocodingWhenNotTopViewController() {
-        navController.addChildViewController(controller)
 
-        replaceRootViewController(with: controller)
-        attemptGeocoding(withText: "12345")
-
-        controller.removeFromParentViewController()
-
-        guard let handler = geocoder.forwardGeocodeAddressCompletionHandler else {
-            return XCTFail("Geocoder should be called with a handler")
-        }
-        handler([placemark], nil)
-
-        XCTAssertFalse(navController.showCalled,
-                       "Navigation controller should not call show on a successful geocoding when location controller is not the top view controller")
-        XCTAssertFalse(controller.performSegueCalled,
-                       "Controller should not perform segue on successful geocoding when not top view controller")
     }
 
-    func testSuccessfulGeocodingWhenTopViewController() {
-        navController.addChildViewController(controller)
-
-        replaceRootViewController(with: navController)
-
-        attemptGeocoding(withText: "12345")
-
-        // Find out why this one won't cooperate
-        delegate.textFieldDidEndEditing!(textField)
-
-        guard let handler = geocoder.forwardGeocodeAddressCompletionHandler else {
-            return XCTFail("Geocoder should be called with a handler")
-        }
-        handler([placemark], nil)
-
-        XCTAssertTrue(navController.showCalled,
-                      "Navigation controller should call show when geocoding is successful")
-        XCTAssertTrue(navController.showController is AnimalCardsViewController,
-                      "Navigation controller should show animal cards controller when geocoding is successful")
-
-        XCTAssertTrue(controller.performSegueCalled,
-                      "Controller should perform segue when geocoding is successful")
-        XCTAssertEqual(controller.performSegueIdentifier, "ShowAnimalCardsViewController",
-                       "Controller should use correct segue identifier to show animal cards controller")
-
-        performSegueSpy?.endSpying()
-        showSpy?.endSpying()
-    }
-
-    func testSavingToUserDefaults() {
-        replaceRootViewController(with: controller)
-        attemptGeocoding(withText: "12345")
-
-        guard let handler = geocoder.forwardGeocodeAddressCompletionHandler else {
-            return XCTFail("Geocoder should be called with a handler")
-        }
-        handler([placemark], nil)
-
-        XCTAssertEqual(
-            SettingsManager.shared.value(forKey: SettingsManager.Key.zipCode) as? String,
-            "80220",
-            "Geocoding should save zip code to settings"
-        )
-    }
-
-    func testTextFieldDoesNotPrepopulateWithStoredZipCodeIfNotAvailable() {
-        SettingsManager.shared.set(value: nil, forKey: .zipCode)
-
-        controller.viewDidLoad()
-        XCTAssertTrue(textField.text!.isEmpty,
-                      "Textfield should not prepopulate from stored zip code if no zip code is stored")
-    }
-
-    func testTextFieldPrepopulatesOnViewDidLoadWithStoredZipCodeIfAvailable() {
-        SettingsManager.shared.set(value: "12345", forKey: .zipCode)
-
-        controller.viewDidLoad()
-        XCTAssertEqual(textField.text, "12345",
-                       "Textfield should prepopulate from stored zip code")
-    }
-
-    func testTextFieldPrepopulatesOnViewWillAppearWithStoredZipCodeIfAvailable() {
-        SettingsManager.shared.set(value: "12345", forKey: .zipCode)
-
-        controller.viewWillAppear(false)
-        XCTAssertEqual(textField.text, "12345",
-                       "Textfield should prepopulate from stored zip code")
-    }
 }
 
 extension LocationControllerTests {
-    func loadComponents() {
-        let storyboard = UIStoryboard(name: "Main", bundle: Bundle(for: LocationController.self))
-        controller = storyboard.instantiateViewController(withIdentifier: "LocationController") as? LocationController
-        controller.loadViewIfNeeded()
-
-        textField = controller.zipCodeField
-        delegate = controller
-        geocoder = controller.geocoder
-    }
-
     func attemptGeocoding(withText text: String) {
-        textField.becomeFirstResponder()
-        textField.text = text
-        textField.resignFirstResponder()
     }
 
 }

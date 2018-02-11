f45b3a8bb4c097502401d4f9eaed7f778601bbdc
joesus <joesusnick@gmail.com>
2018-02-11 15:29:56 -0700

    Adds search workflow for segue identifiers

---
+++ AllisonsCatPalaceTDD/User Interface/SearchWorkflow.swift
@@ -0,0 +1,17 @@
+//
+//  SearchWorkflow.swift
+//  AllisonsCatPalaceTDD
+//
+//  Created by Joe Susnick on 2/11/18.
+//  Copyright © 2018 Joesus. All rights reserved.
+//
+
+import Foundation
+
+typealias SegueIdentifier = String
+
+enum SearchWorkflow {
+    enum SegueIdentifiers {
+        static let performSearch: SegueIdentifier = "PerformSearch"
+    }
+}

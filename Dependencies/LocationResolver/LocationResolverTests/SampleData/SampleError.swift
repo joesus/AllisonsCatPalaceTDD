643a2047e3bbdc4e9ca7a8e8611501e0ce416fcc
joesus <joesusnick@gmail.com>
2018-12-08 16:15:09 -0700

    Adding sample data

---
+++ Dependencies/LocationResolver/LocationResolverTests/SampleData/SampleError.swift
@@ -0,0 +1,11 @@
+//
+//  SampleError.swift
+//  AllisonsCatPalaceTDDTests
+//
+//  Created by Joe Susnick on 12/10/17.
+//  Copyright © 2017 Joesus. All rights reserved.
+//
+
+import Foundation
+
+struct SampleError: Error {}

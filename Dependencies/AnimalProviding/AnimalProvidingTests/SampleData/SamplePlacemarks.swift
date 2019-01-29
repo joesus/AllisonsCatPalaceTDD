3ebbe8d54a14d23dfda9313dba66cc5f6c53eb4e
joesus <joesusnick@gmail.com>
2019-01-28 17:12:10 -0800

    Updating sample placemarks to use valid bundle

--- Dependencies/AnimalProviding/AnimalProvidingTests/SampleData/SamplePlacemarks.swift
+++ Dependencies/AnimalProviding/AnimalProvidingTests/SampleData/SamplePlacemarks.swift
@@ -12,7 +12,7 @@ enum SamplePlacemarks {
     static let denver = loadPlacemark(named: "DenverPlacemark")
 
     static func loadPlacemark(named filename: String) -> CLPlacemark {
-        guard let url = Bundle(for: LocationResolverTests.self)
+        guard let url = Bundle(for: SearchParametersTests.self)
             .url(forResource: filename, withExtension: nil),
             let placemark = NSKeyedUnarchiver.unarchiveObject(withFile: url.path)
                 as? CLPlacemark
